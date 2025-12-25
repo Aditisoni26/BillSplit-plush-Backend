d16dc54b38bb9cb6fcb83de74fab5db561225ef8
Aditisoni26 <aditisoni2617@gmail.com>
2025-12-25 18:31:55 +0530

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ["http://localhost:5173", "http://localhost:5174", "https://your-frontend.vercel.app"],
+        origin: "*",
         credentials: true,
         allowedHeaders: ["Content-Type", "Authorization"]
     })
