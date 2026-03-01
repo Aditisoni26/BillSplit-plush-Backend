2a4af8b76964b320db107f01a3ec00e4ff18bab8
Aditi soni <aditisoni2617@gmail.com>
2026-03-01 13:00:29 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,7 @@ module.exports = function(req, res, next) {
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        // ✅ IMPORTANT FIX
+     
         req.user = decoded.id;
 
         next();
